180a4e59c2c0e5807ff81ca5ba1cd004adc282c8
Andjela <golubova.andjela@gmail.com>
2019-04-16 11:29:53 +0200

    Initial commit

---
+++ program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	//TODO
+    //cout<<"Andjela Golubova"<<endl;
+}
+
+void printMyIndex () {
+	//TODO
+    //cout<<"171089"<<endl;
+}
+
+int main () {
+	//TODO
+}
